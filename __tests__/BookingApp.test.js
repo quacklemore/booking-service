bc42bde7ef0202a5809b29155e8c2b65a4ec748b
Luke Eastman <LukeAEastman@gmail.com>
2020-09-30 15:10:31 -0700

    create some tests for booking module

---
+++ __tests__/BookingApp.test.js
@@ -0,0 +1,55 @@
+const puppeteer = require('puppeteer');
+
+import TestRenderer from 'react-test-renderer';
+import React from 'react';
+import ReactTestUtils from 'react-dom/test-utils';
+import { configure, mount, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+configure({ adapter: new Adapter() });
+
+import BookingApp from '../client/components/BookingApp.jsx';
+import Booking from '../client/components/Booking.jsx';
+import CalendarDay from '../client/components/CalendarDay.jsx';
+import Calendar from '../client/helpers/calendar.js';
+
+describe('test booking component', () => {
+
+  test('sanity check', () => {
+    expect(true).toBe(true);
+  });
+
+
+  test('it should call the function to toggle calendar visibility', ()=> {
+
+    let wrapper = shallow(<Booking />)
+    const setShowCalendars = jest.fn();
+    wrapper.setProps({
+      setShowCalendars: setShowCalendars
+    })
+    wrapper.getElement().props.children[0].props.onClick()
+
+    expect(setShowCalendars).toBeCalled();
+    wrapper.getElement().props.children[1].props.onClick()
+    expect(setShowCalendars).toHaveBeenCalledTimes(2);
+  });
+
+  test('it should display the check in date if it exists', () => {
+    let wrapper = mount(<Booking />)
+    const setShowCalendars = jest.fn();
+    var checkIn = new Date();
+    let day = Calendar.prototype.getDay(checkIn.getDay())
+    let checkInDisplay = `${day}, ${checkIn.getMonth()}/${checkIn.getDate()}/${checkIn.getFullYear()}`
+    wrapper.setProps({
+      setShowCalendars: setShowCalendars,
+      checkIn: checkIn
+    });
+    expect(wrapper.find('span').getElements()[2].props.children).toBe(checkInDisplay)
+
+  })
+
+  test('it should have a day component', () => {
+    let wrapper = mount(<BookingApp />);
+    expect(wrapper.find('CalendarDay')).toBeTruthy();
+  })
+
+})
---
+++ client/components/BookingApp.jsx
@@ -0,0 +1,69 @@
+import React, {useState, useEffect} from 'react';
+import ReactDOM from 'react-dom';
+import styled from 'styled-components'
+import axios from 'axios';
+import moment from 'moment';
+
+import CalendarMonth from './CalendarMonth.jsx';
+import Booking from './Booking.jsx';
+
+import Calendar from '../helpers/calendar.js';
+
+
+const Calendars = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  position: relative;
+  box-shadow: 0 2px 4px 0 rgba(0,0,0,.25);
+`;
+
+
+const StyledBookingApp = styled.div`
+  position: relative;
+  height: 50%;
+  width: 40%;
+  font-family: 'Trip Sans', Arial, sans-serif !important;
+`;
+
+
+const BookingApp = () => {
+  const [trips, setTrips] = useState([]);
+  const [month1, setMonth1] = useState((new Date()).getMonth());
+  const [month2, setMonth2] = useState(month1 + 1);
+  const [year, setYear] = useState((new Date()).getFullYear())
+  const [showCalendars, setShowCalendars] = useState(true);
+  const [checkIn, setCheckIn] = useState();
+
+  useEffect(() => {
+    axios({
+      url: '/api/trips/0',
+      method: 'get',
+    })
+    .then((result) => {
+      setTrips(result.data)
+    })
+  },[]);
+
+
+
+  if (!showCalendars) {
+    return (
+      <StyledBookingApp>
+        <Booking setShowCalendars={setShowCalendars} showCalendars={showCalendars} checkIn={checkIn}/>
+      </StyledBookingApp>
+    );
+  } else {
+    return (
+      <StyledBookingApp>
+        <Booking setShowCalendars={setShowCalendars} showCalendars={showCalendars} checkIn={checkIn}/>
+        <Calendars>
+          <CalendarMonth month={month1} year={year} setCheckIn={setCheckIn} month1={true}/>
+          <CalendarMonth month={month2} year={year} setCheckIn={setCheckIn} month1={false}/>
+        </Calendars>
+      </StyledBookingApp>
+    );
+  }
+
+}
+
+export default BookingApp;
