f6ce7952e9814c5a75ef47a6e42a74c1bec449fa
Luke Eastman <LukeAEastman@gmail.com>
2021-05-06 15:15:16 -0700

    render month border based on styled component prop instead of using separate components

--- client/components/CalendarMonth.jsx
+++ client/components/CalendarMonth.jsx
@@ -8,7 +8,7 @@ import Calendar from '../helpers/calendar.js';
 
 const Month = styled.div`
   border-color: #e0e0e0;
-  border-width: 0 0 0 2px;
+  border-width: ${props => props.hasBorder ?  '0 0 0 2px' : '0 0 0 0'};
   border-style: solid;
   padding: 0 16px;
   margin: 12px 10px 12px -15px;
@@ -32,51 +32,17 @@ var CalendarMonth = ({month, year, setCheckInDate, setCheckOutDate, month1, lowD
   let cal = new Calendar();
   let weeks = cal.daysInWeeks(month, year);
   let first = cal.firstDay(month,year);
-if (!month1) {
-    return (
-      <Month>
-        <MonthHeading>
-        {`${cal.monthName(month)} ${year}`}
-        </MonthHeading>
-        <DaysOfTheWeek />
-        { weeks.map((week, index) => <CalendarWeek weeks={weeks} start={(index === 0) ? first : 0} week={index} key={index}  month={month} year={year} lowDays={lowDays} setCheckInDate={setCheckInDate} setCheckOutDate={setCheckOutDate}  setCheckOutDate={setCheckOutDate} checkInPicker={checkInPicker} checkOutPicker={checkOutPicker} checkInDate={checkInDate} checkOutDate={checkOutDate}/>)}
-      </Month>
-    );
- } else {
+
+
   return (
-    <NoBorderMonth>
+    <Month hasBorder={!month1}>
       <MonthHeading>
       {`${cal.monthName(month)} ${year}`}
       </MonthHeading>
       <DaysOfTheWeek />
-      { weeks.map((week, index) => <CalendarWeek weeks={weeks} start={(index === 0) ? first : 0} week={index} key={index}  month={month} year={year} lowDays={lowDays} setCheckInDate={setCheckInDate} setCheckOutDate={setCheckOutDate} checkInPicker={checkInPicker} checkOutPicker={checkOutPicker} checkInDate={checkInDate} checkOutDate={checkOutDate}/> )}
-    </NoBorderMonth>
+      { weeks.map((week, index) => <CalendarWeek weeks={weeks} start={(index === 0) ? first : 0} week={index} key={index}  month={month} year={year} lowDays={lowDays} setCheckInDate={setCheckInDate} setCheckOutDate={setCheckOutDate}  setCheckOutDate={setCheckOutDate} checkInPicker={checkInPicker} checkOutPicker={checkOutPicker} checkInDate={checkInDate} checkOutDate={checkOutDate}/>)}
+    </Month>
   );
- }
 }
 
 export default CalendarMonth;
-
-{/* <Month>
-<thead>
-  <tr><td>{`${cal.monthName(month)} ${year}`}</td></tr>
-  <DaysOfTheWeek />
-</thead>
-<tbody>
-  { weeks.map((week, index) => <CalendarWeek weeks={weeks} start={(index === 0) ? first : 0} week={index} key={index} setCheckIn={setCheckIn} month={month} year={year}/> )}
-</tbody>
-</Month>
-);
-} else {
-return (
-<NoBorderMonth>
-<thead>
-<tr><td>{`${cal.monthName(month)} ${year}`}</td></tr>
-  <DaysOfTheWeek />
-</thead>
-<tbody>
-  { weeks.map((week, index) => <CalendarWeek weeks={weeks} start={(index === 0) ? first : 0} week={index} key={index} setCheckIn={setCheckIn} month={month} year={year}/> )}
-</tbody>
-</NoBorderMonth>
-)
-} */}
